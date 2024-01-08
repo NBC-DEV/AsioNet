b2074a8524749169b50faf32b780d8618c7b1056
LewisKulen <707335917@qq.com>
2024-01-08 20:07:35 +0800

    clear sth

--- src/TcpConn.h
+++ src/TcpConn.h
@@ -7,46 +7,7 @@
 
 namespace AsioNet
 {
-	
 	// doc:https://www.boost.org/doc/libs/1_84_0/doc/html/boost_asio/reference/ip__tcp/socket.html
-
-	// 如果以接口的形式返回给上面，我应该如何做到在使用接口的时候其指针的有效性得到保证，shared_ptr?
-	struct AN_INTERFACE IANConn : public boost::noncopyable
-	{
-		virtual bool Write(const char *acData, int iSize) = 0;
-		virtual bool Read() = 0;
-	};
-
-	struct IConnectHandler
-	{
-		virtual void operator()(const NetErr &error) = 0;
-		virtual ~IConnectHandler(){};
-	};
-	struct IAcceptHandler
-	{
-		virtual void operator()(const NetErr &error) = 0;
-		virtual ~IAcceptHandler(){};
-	};
-	struct IWriteHandler
-	{
-		virtual void operator()(const NetErr &error, std::size_t bytes_transferred) = 0;
-		virtual ~IWriteHandler(){};
-	};
-	struct IReadHandler
-	{
-		virtual void operator()(const NetErr &error, std::size_t bytes_transferred) = 0;
-		virtual ~IReadHandler(){};
-	};
-
-	struct AN_INTERFACE IANCompleteHandler
-	{
-		virtual ~IANCompleteHandler() {}
-		virtual void connect_handler(const NetErr &error) = 0;
-		virtual void accept_handler(const NetErr &error) = 0;
-		virtual void read_handler(const NetErr &error, std::size_t bytes_transferred) = 0;
-		virtual void write_handler(const NetErr &error, std::size_t bytes_transferred) = 0;
-	};
-
 	// 只能由client/server创建
 	class TcpConn : public std::enable_shared_from_this<TcpConn>
 	{
