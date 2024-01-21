52573d1c0c9b9b86173ceda8a1347f84c604d648
LewisKulen <707335917@qq.com>
2024-01-21 14:57:14 +0800

    修复DefaultPoller在Pop时候的错误

--- main.cpp
+++ main.cpp
@@ -20,16 +20,18 @@ struct TestServerHandler :public AsioNet::IEventHandler {
 
 class TestServer :public AsioNet::IEventHandler {
 public:
-	TestServer() :netMgr(2, this)
+	TestServer() :netMgr(8, this)
 	{
-		std::thread([this]() {
-			while (true) {
-				netMgr.Update();
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
-			}
-		}).detach();
 		netMgr.Serve(8888);
 		std::cout << "listen at:8888" << std::endl;
+	}
+	void Update()
+	{
+		while (true) {
+			netMgr.Update();
+
+			// std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
 	}
 	void AcceptHandler(AsioNet::NetKey k) override
 	{
@@ -51,11 +53,12 @@ public:
 	void RecvHandler(AsioNet::NetKey k, const std::string& data) override
 	{
 		auto remote = AsioNet::NetKey2Addr(k);
-		printf("recv from[%s:%d]:%s\n", remote.ip.c_str(), remote.port, data.c_str());
+		// printf("recv from[%s:%d]:len[%d]\n", remote.ip.c_str(), remote.port, data.length());
 		// echo
-		printf("echo[%s:%d]:%s\n", remote.ip.c_str(), remote.port, data.c_str());
 		bool succ = netMgr.Send(k, data.c_str(), data.length());
-		printf("echo succ[%d]\n", succ);
+		if (!succ) {
+			printf("echo[%s:%d]:len[%d],succ[%d]\n", remote.ip.c_str(), remote.port, data.length(), succ);
+		}
 	}
 	void ErrorHandler(AsioNet::NetKey, const AsioNet::NetErr& err) override
 	{
@@ -67,32 +70,42 @@ private:
 
 struct TestClient :public AsioNet::IEventHandler {
 public:
-	TestClient() :netMgr(2, this), m_conn(0)
+	TestClient() :netMgr(2, this), m_conn(0), hasErr(false)
 	{
 		std::thread([this]() {
 			while (true) {
 				netMgr.Update();
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				// 这里只是为了测试方便，另外开了个线程，实际使用时不要这么用
+				// 不然主线程析构了还在子线程的逻辑就G了
 			}
-		}).detach();
+			}).detach();
 
-		netMgr.Connect("127.0.0.1", 8888, 100);
+			netMgr.Connect("127.0.0.1", 8888, 100);
 	}
-	void Update()
+	void DoTest()
 	{
 		while (m_conn == 0) {
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 		}
-		for (int i = 1;; ++i)
+
+		int n = AsioNet::AN_MSG_MAX_SIZE;
+		std::string data;
+		for (int i = 0; i < n; ++i)
 		{
-			auto s = std::to_string(i);
-			printf("send len[%d]:data[%s]\n", s.length(), s.c_str());
-			bool succ = netMgr.Send(m_conn, s.c_str(), s.length());
-			if (!succ)
+			data.push_back(char('a' + i % 26));
+		}
+
+		int mod = 1'0000;
+		for (int trans = 1; trans <= data.length(); ++trans)
+		{
+			auto s = data.substr(0, trans);
 			{
-				printf("send err\n");
+				_lock_guard_(lock);
+				que.push(s);
 			}
-			std::this_thread::sleep_for(std::chrono::seconds(3));
+			bool succ = netMgr.Send(m_conn, s.c_str(), s.length());
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
 		}
 	}
 
@@ -119,7 +132,16 @@ public:
 	void RecvHandler(AsioNet::NetKey k, const std::string& data) override
 	{
 		auto remote = AsioNet::NetKey2Addr(k);
-		printf("recv from[%s:%d]:%s\n", remote.ip.c_str(), remote.port, data.c_str());
+		{
+			_lock_guard_(lock);
+			if (que.front() != data)
+			{
+				hasErr = true;
+				//printf("check error:send [%s],recv[%s]!\n",que.front().c_str(),data.c_str());
+			}
+			que.pop();
+		}
+		// printf("recv from[%s:%d]:%s\n", remote.ip.c_str(), remote.port, data.c_str());
 		// echo
 		// netMgr.Send(k, data.c_str(), data.length());
 	}
@@ -127,16 +149,38 @@ public:
 	{
 		std::cout << err.message().c_str() << std::endl;
 	}
+	std::atomic<bool> hasErr;
+
 private:
 	AsioNet::TcpNetMgr netMgr;
 	std::atomic<AsioNet::NetKey> m_conn;
+
+	std::mutex lock;
+	std::queue<std::string > que;
 };
+
+// 测试结果：
+// 客户端内存始终保持在2MB左右，即使后期一直发送长度在上万长度的数据（此时收发缓冲区基本都被堆到了最大了）
+// 服务器每个客户端大概占用，0.5M左右的内存，内存消耗都堆积在消息队列中，就看消息处理速度有多快
 
 int main()
 {
-	// TestServer s;
+	TestServer s;
+	s.Update();
+	//TestClient c;
+	//c.DoTest();
+	//printf("client test finish:succ[%d]\n",!(c.hasErr));
 
-	TestClient c;
-	c.Update();
 	return 0;
 }
+
+/*
+send[abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwx]
+recv[]!
+
+send[abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy]
+recv[abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxa]!
+
+
+
+*/
