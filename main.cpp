75c63edb16202414d1aa1ca3d91a72a916f2947b
LewisKulen <707335917@qq.com>
2024-03-01 18:03:06 +0800

    删除无效代码

--- main.cpp
+++ main.cpp
@@ -12,17 +12,7 @@
 #include <atomic>
 #include <mutex>
 
-// #include <boost/thread.hpp>
-
-struct TestServerHandler :public AsioNet::IEventHandler {
-	void AcceptHandler(AsioNet::NetKey) {};
-	void ConnectHandler(AsioNet::NetKey) {};
-	void DisconnectHandler(AsioNet::NetKey) {};
-	void RecvHandler(AsioNet::NetKey, const char* data, size_t trans) {};
-	void RecvHandler(AsioNet::NetKey, const std::string&) {};
-};
-
-class TestServer :public AsioNet::IEventHandler {
+class TestServer {
 public:
 	TestServer() :netMgr(8, this)
 	{
@@ -37,47 +27,19 @@ public:
 			// std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
 	}
-	void AcceptHandler(AsioNet::NetKey k) override
-	{
-		auto remote = AsioNet::NetKey2Addr(k);
-		auto local = AsioNet::NetKey2ServerKey(k);
-		printf("accept[%d]:[%s:%d]\n", local, remote.ip.c_str(), remote.port);
-	}
-	void ConnectHandler(AsioNet::NetKey k) override
-	{
-		auto remote = AsioNet::NetKey2Addr(k);
-		printf("connect to[%s:%d]\n", remote.ip.c_str(), remote.port);
-	}
-	void DisconnectHandler(AsioNet::NetKey k) override
-	{
-		auto remote = AsioNet::NetKey2Addr(k);
-		printf("disconnect from[%s:%d]\n", remote.ip.c_str(), remote.port);
-	}
-	void RecvHandler(AsioNet::NetKey, const char* data, size_t trans) override {};
-	void RecvHandler(AsioNet::NetKey k, const std::string& data) override
-	{
-		auto remote = AsioNet::NetKey2Addr(k);
-		// printf("recv from[%s:%d]:len[%d]\n", remote.ip.c_str(), remote.port, data.length());
-		// echo
-		bool succ = netMgr.Send(k, data.c_str(), data.length());
-		if (!succ) {
-			printf("echo[%s:%d]:len[%lld],succ[%d]\n", remote.ip.c_str(), remote.port, data.length(), succ);
-		}
-	}
-
 private:
 	AsioNet::TcpNetMgr netMgr;
 };
 
-struct TestClient :public AsioNet::IEventHandler {
+struct TestClient{
 public:
 	TestClient() :netMgr(2, this), m_conn(0), hasErr(false)
 	{
 		std::thread([this]() {
 			while (true) {
 				netMgr.Update();
-				// ÕâÀïÖ»ÊÇÎªÁË²âÊÔ·½±ã£¬ÁíÍâ¿ªÁË¸öÏß³Ì£¬Êµ¼ÊÊ¹ÓÃÊ±²»ÒªÕâÃ´ÓÃ
-				// ²»È»Ö÷Ïß³ÌÎö¹¹ÁË»¹ÔÚ×ÓÏß³ÌµÄÂß¼­¾ÍGÁË
+				// ï¿½ï¿½ï¿½ï¿½Ö»ï¿½ï¿½Îªï¿½Ë²ï¿½ï¿½Ô·ï¿½ï¿½ã£¬ï¿½ï¿½ï¿½â¿ªï¿½Ë¸ï¿½ï¿½ß³Ì£ï¿½Êµï¿½ï¿½Ê¹ï¿½ï¿½Ê±ï¿½ï¿½Òªï¿½ï¿½Ã´ï¿½ï¿½
+				// ï¿½ï¿½È»ï¿½ï¿½ï¿½ß³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ë»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ß³Ìµï¿½ï¿½ß¼ï¿½ï¿½ï¿½Gï¿½ï¿½
 			}
 			}).detach();
 
@@ -109,38 +71,6 @@ public:
 
 		}
 	}
-
-	void AcceptHandler(AsioNet::NetKey k) override
-	{}
-	void ConnectHandler(AsioNet::NetKey k) override
-	{
-		auto remote = AsioNet::NetKey2Addr(k);
-		printf("connect to[%s:%d]\n", remote.ip.c_str(), remote.port);
-		m_conn = k;
-	}
-	void DisconnectHandler(AsioNet::NetKey k) override
-	{
-		m_conn = AsioNet::INVALID_NET_KEY;
-		auto remote = AsioNet::NetKey2Addr(k);
-		printf("disconnect from[%s:%d]\n", remote.ip.c_str(), remote.port);
-		netMgr.Connect("127.0.0.1", 8888, 100);
-	}
-	void RecvHandler(AsioNet::NetKey, const char* data, size_t trans) override {};
-	void RecvHandler(AsioNet::NetKey k, const std::string& data) override
-	{
-		auto remote = AsioNet::NetKey2Addr(k);
-		{
-			_lock_guard_(lock);
-			if (que.front() != data)
-			{
-				hasErr = true;
-			}
-			que.pop();
-		}
-		// printf("recv from[%s:%d]:%s\n", remote.ip.c_str(), remote.port, data.c_str());
-	}
-	std::atomic<bool> hasErr;
-
 private:
 	AsioNet::TcpNetMgr netMgr;
 	std::atomic<AsioNet::NetKey> m_conn;
@@ -149,38 +79,27 @@ private:
 	std::queue<std::string > que;
 };
 
-class KcpPoller:public AsioNet::IEventPoller{
-public:
-	void PushAccept(AsioNet::NetKey k) override
-	{}
-	void PushConnect(AsioNet::NetKey k) override
-	{}
-	void PushDisconnect(AsioNet::NetKey k) override
-	{}
-	void PushRecv(AsioNet::NetKey k, const char *data, size_t trans) override
-	{}
-};
-
 int main()
 {
 	// TestServer s;
 	// s.Update();
-	//TestClient c;
-	//c.DoTest();
-	//printf("client test finish:succ[%d]\n",!(c.hasErr));
-	AsioNet::io_ctx ctx;
-	std::thread th([&ctx]{
-		while(true){
-			ctx.run();
-		}
-	});
-	KcpPoller poller;
-	auto client = std::make_shared<AsioNet::KcpConn>(ctx,&poller);
-	client->Write("12345",5);
+	// TestClient c;
+	// c.DoTest();
+	// printf("client test finish:succ[%d]\n",!(c.hasErr));
+	
+	// AsioNet::io_ctx ctx;
+	// std::thread th([&ctx]{
+	// 	while(true){
+	// 		ctx.run();
+	// 	}
+	// });
+	// KcpPoller poller;
+	// auto client = std::make_shared<AsioNet::KcpConn>(ctx,&poller);
+	// client->Write("12345",5);
 
-	auto server = std::make_shared<AsioNet::KcpConn>(ctx,&poller);
+	// auto server = std::make_shared<AsioNet::KcpConn>(ctx,&poller);
 	
 	
-	th.join();
+	// th.join();
 	return 0;
 }
